1779123c349ca369d672abef0169681e9be77342
Cruz Ramirez <cruz_ramirez@ymail.com>
2019-12-23 22:34:40 -0800

    added accordion w/o content

--- wordpress/wp-content/themes/MVMT/footer.php
+++ wordpress/wp-content/themes/MVMT/footer.php
@@ -1,5 +1,6 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
+			<!--Black lip above the footer-->
 			<div class="row d-flex flex-row justify-content-around align-items-center bg_dark">
 				<div class="col-md-3 d-flex value-propositions flex-row align-items-center justify-content-center">
 					<img src="//cdn.shopify.com/s/files/1/0377/2037/t/140/assets/proposition-img-0.svg?3622083" alt="white illustrated plane" class="" height="43" width="40">
@@ -18,7 +19,49 @@
 					<p>100% SAFE & SECURE <br>CHECKOUT</p>
 				</div>
 			</div>
+			<!--END of Black lip above the footer-->
+			<!--Footer Hidden after medium up-->
+			<div class="row footer-row d-flex flex-column d-md-none d-lg-block  justify-content-center">
+				<div class="row d-flex flex-column justify-content-center text-center">
+					<a href="#">Logo</a>
+					<ul>
+						<li><a href="mailto:support@mvmt.com">support@mvmt.com</a></li>
+						<li>Call Toll-Free <br><a href="tel:8885070220">+1 (888) 507-0220</a></li>
+						<li>M-T 8:30-2 pm & F 9-1 pm PST</li>
+					</ul>
+				</div>
+				<div class="accordion-row row d-flex m-1">
+					<button class="accordion">SHOP MEN'S</button>
+					<div class="panel">
+						<ul class="accordion-content" data-accordion-content="" style="display: block;">
+							<li><a href="/collections/all-mens-watches">Watches</a></li>
+							<li><a href="/collections/mens-sunglasses">Sunglasses</a></li>
+							<li><a href="/collections/mens-glasses">Everscroll</a></li>
+							<li><a href="/collections/mens-jewelry">Jewelry</a></li>
+							<li><a href="/pages/mens-strap-guide">Straps</a></li>
+						</ul>
+					</div>
 
+					<button class="accordion">SHOP WOMEN'S</button>
+					<div class="panel">
+					<p>Lorem ipsum...</p>
+					</div>
+
+					<button class="accordion">CUSTOMER SERVICE</button>
+					<div class="panel">
+					<p>Lorem ipsum...</p>
+					</div>
+
+					<button class="accordion">MORE</button>
+					<div class="panel">
+					<p>Lorem ipsum...</p>
+					</div>
+				</div>
+			</div>
+			<!--END-->
+			<div class="row footer-row d-none d-md-block d-lg-none">
+				<h1>Large</h1>
+			</div>
 			</footer>
 			<!-- /footer -->
 		</div>
--- wordpress/wp-content/themes/MVMT/js/scripts.js
+++ wordpress/wp-content/themes/MVMT/js/scripts.js
@@ -1,11 +1,30 @@
 (function ($, root, undefined) {
-	
+
 	$(function () {
-		
+
 		'use strict';
-		
+
 		// DOM ready, take it away
-		
+		// Followed Tutorial on Accordions
+		var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			/* Toggle between adding and removing the "active" class,
+			to highlight the button that controls the panel */
+			this.classList.toggle("active");
+
+			/* Toggle between hiding and showing the active panel */
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+			panel.style.display = "none";
+			} else {
+			panel.style.display = "block";
+			}
+		});
+		}
+		// END accordion tutorial
 	});
-	
+
 })(jQuery, this);
