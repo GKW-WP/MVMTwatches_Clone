012fa4f1eff6447bcd7a8de83835b84f5f4646e1
Cruz Ramirez <cruz_ramirez@ymail.com>
2019-12-26 11:44:35 -0800

    updated footer scss

--- wordpress/wp-content/themes/MVMT/footer.php
+++ wordpress/wp-content/themes/MVMT/footer.php
@@ -21,7 +21,7 @@
 			</div>
 			<!--END of Black lip above the footer-->
 			<!--Footer Hidden after medium up-->
-			<div class="row footer-row d-flex flex-column d-md-none d-lg-block  justify-content-center">
+			<div class="row footer-row d-flex flex-column d-md-none justify-content-center">
 				<div class="row d-flex flex-column justify-content-center text-center">
 					<a href="#">Logo</a>
 					<ul>
@@ -31,35 +31,82 @@
 					</ul>
 				</div>
 				<div class="accordion-row row d-flex m-1">
-					<button class="accordion">SHOP MEN'S</button>
-					<div class="panel">
-						<ul class="accordion-content" data-accordion-content="" style="display: block;">
-							<li><a href="/collections/all-mens-watches">Watches</a></li>
-							<li><a href="/collections/mens-sunglasses">Sunglasses</a></li>
-							<li><a href="/collections/mens-glasses">Everscroll</a></li>
-							<li><a href="/collections/mens-jewelry">Jewelry</a></li>
-							<li><a href="/pages/mens-strap-guide">Straps</a></li>
-						</ul>
+					<!-- Accordion footer -->
+					<div class="accordion" id="accordionfooter">
+						<!-- Accordion Card -->
+						<div class="card">
+							<button class="btn btn-link text-left" type="button" data-toggle="collapse" data-target="#MenLinks">
+								<div class="card-header" id="MenHeading">
+									Shop Men's
+								</div>
+							</button>
+							<div id="MenLinks" class="collapse" aria-labelledby="#MenHeading" data-parent="#accordionfooter">
+								<div class="card-body">
+									<ul>
+										<li><a href="#">Watches</a></li>
+										<li><a href="#">Sunglasses</a></li>
+										<li><a href="#">Everscroll</a></li>
+										<li><a href="#">Jewelry</a></li>
+										<li><a href="#">Straps</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!-- Accordion Card End -->
+						<!-- Accordion Card -->
+						<div class="card">
+							<button class="btn btn-link text-left" type="button" data-toggle="collapse" data-target="#WomenLinks">
+								<div class="card-header" id="WomenHeading">
+									Shop Women's
+								</div>
+							</button>
+							<div id="WomenLinks" class="collapse" aria-labelledby="#WomenHeading" data-parent="#accordionfooter">
+								<div class="card-body">
+									<ul>
+										<li><a href="/collections/womens-best-sellers">Watches</a></li>
+										<li><a href="/collections/womens-sunglasses">Sunglasses</a></li>
+										<li><a href="/collections/womens-glasses">Everscroll</a></li>
+										<li><a href="/collections/womens-jewelry">Jewelry</a></li>
+										<li><a href="/pages/womens-strap-guide">Straps</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!-- Accordion Card End -->
+						<!-- Accordion Card -->
+						<div class="card">
+							<button class="btn btn-link text-left" type="button" data-toggle="collapse" data-target="#CustomerServiceLinks">
+								<div class="card-header" id="CustomerServiceHeader">
+									Customer Service
+								</div>
+							</button>
+							<div id="CustomerServiceLinks" class="collapse" aria-labelledby="#CustomerServiceHeader" data-parent="#accordionfooter">
+								<div class="card-body">
+									More Stuff
+								</div>
+							</div>
+						</div>
+						<!-- Accordion Card End -->
+						<!-- Accordion Card -->
+						<div class="card">
+							<button class="btn btn-link text-left" type="button" data-toggle="collapse" data-target="#MoreLinks">
+								<div class="card-header" id="MoreHeader">
+									More
+								</div>
+							</button>
+							<div id="MoreLinks" class="collapse" aria-labelledby="#MoreHeader" data-parent="#accordionfooter">
+								<div class="card-body">
+									Even More Stuff
+								</div>
+							</div>
+						</div>
+						<!-- Accordion Card End -->
 					</div>
-
-					<button class="accordion">SHOP WOMEN'S</button>
-					<div class="panel">
-					<p>Lorem ipsum...</p>
-					</div>
-
-					<button class="accordion">CUSTOMER SERVICE</button>
-					<div class="panel">
-					<p>Lorem ipsum...</p>
-					</div>
-
-					<button class="accordion">MORE</button>
-					<div class="panel">
-					<p>Lorem ipsum...</p>
-					</div>
+					<!-- Accordion Footer End -->
 				</div>
 			</div>
 			<!--END-->
-			<div class="row footer-row d-none d-md-block d-lg-none">
+			<div class="row footer-row d-none d-md-block">
 				<h1>Large</h1>
 			</div>
 			</footer>
