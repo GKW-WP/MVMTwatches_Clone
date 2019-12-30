0e3c35014518eae06f99cda38474a9eaf2001362
Cruz Ramirez <cruz_ramirez@ymail.com>
2019-12-30 13:52:45 -0800

    added 2 column row for promos

---
+++ wordpress/wp-content/themes/MVMT/page-home-promo.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Template Name: Promo-Page
+ * Template Post Type: page
+ */
+get_header(); ?>
+	<!-- Promotion Dual Column -->
+	<div class="row promos d-flex flex-row justify-content-around">
+		<div class="col-md-5 d-flex justify-content-center align-items-end" style="background-image: url(http://mvmt.imgix.net/s/files/1/0377/2037/files/tile_neon_M_2_4fd9a86c-266e-4cfc-b7b0-2250f7edc845_900x.progressive.jpg?v=1577211137);">
+			<div class="promo-link text-center">
+				<a href="#" class="btn">Men's Last Chance</a>
+			</div>
+		</div>
+		<div class="col-md-5 d-flex justify-content-center align-items-end" style="background-image: url(https://mvmt.imgix.net/s/files/1/0377/2037/files/tile_neon_2_900x.jpg?v=1577123576);">
+			<div class="promo-link text-center">
+				<a href="#" class="btn">Womens Last Chance</a>
+			</div>
+		</div>
+	</div>
+	<!-- End of Promotion DUal Column -->
+	<!-- Watch Slider -->
+	<div class="row">
+		Slider :D
+	</div>
+	<!-- End of Watch Slider -->
+	<!-- Three Column -->
+	<!-- End of Three Column -->
+<?php get_footer(); ?>
